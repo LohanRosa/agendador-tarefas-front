e97d86a4e02552b489c08a1019bfae1649cd6874
Lohan Rosa <lohan.rosa@smilatam.net>
2025-12-10 10:06:51 -0300

    create input password component

--- src/app/pages/register/register.ts
+++ src/app/pages/register/register.ts
@@ -5,22 +5,18 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
 import { MatIconModule } from '@angular/material/icon';
+import { PasswordField } from '../../shared/components/password-field/password-field';
 
 
 @Component({
   selector: 'app-register',
   imports: [MatCardModule, MatButtonModule, MatFormFieldModule, 
-    MatInputModule, MatSelectModule, MatIconModule],
+    MatInputModule, MatSelectModule, MatIconModule,PasswordField],
     changeDetection: ChangeDetectionStrategy.OnPush,
     encapsulation: ViewEncapsulation.None,
   templateUrl: './register.html',
   styleUrl: './register.scss',
 })
 export class Register {
-  hide = signal(true);
-  clickEvent(event: MouseEvent) {
-    this.hide.set(!this.hide());
-    event.stopPropagation();
-  }
 
 }
